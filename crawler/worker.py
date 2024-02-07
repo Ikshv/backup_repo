c8bf1f34b10335e7ace4aa938d06cade2ee707ed
IKshv <ikshvartsman@gmail.com>
2024-02-07 00:27:07 -0800

    forgot to change port

--- crawler/worker.py
+++ crawler/worker.py
@@ -1,17 +1,19 @@
 from threading import Thread
-
 from inspect import getsource
 from utils.download import download
 from utils import get_logger
 import scraper
 import time
-
+import threading
 
 class Worker(Thread):
-    def __init__(self, worker_id, config, frontier):
-        self.logger = get_logger(f"Worker-{worker_id}", "Worker")
+    def __init__(self, worker_id, config, frontier, result):
+        self.logger = get_logger(f"Worker-{worker_id}", "WORKER")
         self.config = config
         self.frontier = frontier
+        self.result = result
+        self.shutdown_request = False
+        self.shutdown_lock = threading.Lock()
         # basic check for requests in scraper
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper.py"
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
@@ -27,8 +29,26 @@ class Worker(Thread):
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper.scraper(tbd_url, resp)
+            scraped_urls = scraper.scraper(tbd_url, resp, self.result)
+            if len(self.result.visited_urls) % 5 == 0:
+                print("saving to file -- 100")
+                self.result.write_to_file('results.csv')
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
+
+    def request_shutdown(self):
+        with self.shutdown_lock:
+            self.shutdown_request = True
+
+    def check_shutdown_request(self):
+        with self.shutdown_lock:
+            return self.shutdown_request
+        
+    def graceful_shutdown(self):
+        self.result.write_to_file('results.csv')
+        self.request_shutdown()
+        self.join()
+        self.logger.info(f"Worker-{self.ident} has shutdown gracefully.")
+        return
